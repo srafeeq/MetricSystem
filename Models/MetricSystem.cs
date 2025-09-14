58b203b2e3ff3704f8060d520e7c2eeec208e026
srafeeq <srafeeq@govst.edu>
2025-09-14 03:43:31 -0500

    Update MetricSystem.cs

--- Models/MetricSystem.cs
+++ Models/MetricSystem.cs
@@ -4,10 +4,12 @@ namespace MetricSystem.Models
 {
     public class FahrenheitModel
     {
-        [Required(ErrorMessage = "Fahrenheit value is required")]
+        [Required(ErrorMessage = "Fahrenheit Value is required")]
         [Range(-100, 200, ErrorMessage = "Enter a value between -100 and 200 Fahrenheit")]
+        [Display(Name = "Fahrenheit Value")]  
         public double Fahrenheit { get; set; }
 
-        public double? Celsius { get; set; }
+        [Display(Name = "Celsius Value")]  
+        public double? Celsius { get; set; } 
     }
 }
